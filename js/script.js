f588a2cb720562ed17bea23aaee87453b91a3b17
Chandan sharma <c.sharma@uleth.ca>
2023-11-07 11:28:54 -0700

    added methods to translate whole text

--- js/script.js
+++ js/script.js
@@ -3,25 +3,29 @@ const xhr = new XMLHttpRequest();
 const baseurl = "https://api.funtranslations.com/translate/pirate.json?text=";
 var heading = document.getElementById("firstHeading");
 
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(async function(request, sender, sendResponse) {
     if (request.action === 'translate') {
       console.log('working');
-      var query;
-      query = heading.textContent;
-      const url = baseurl + query;
+      var query = document.body.innerText;
+      var queries = query.match(/.{1,500}/g); // Split the text into chunks of 500 characters each
+      console.log(queries);
 
-      xhr.open("GET", url);
-      xhr.send(null);
+      for (let i = 0; i < queries.length; i++) {
+        const url = baseurl + queries[i];
 
-      xhr.onload = function() {
-        console.log("api gave response");
-        txt = JSON.parse(xhr.responseText);
-        console.log(txt);
-        console.log(txt.contents.translated);
-        heading.textContent = txt.contents.translated;
+        xhr.open("GET", url);
+        xhr.send(null);
+
+        await new Promise(resolve => {
+          xhr.onload = function() {
+            txt = JSON.parse(xhr.responseText);
+            console.log(txt);
+            console.log(txt.contents.translated);
+            document.body.innerText.textContent = txt.contents.translated;
+            resolve();
+          }
+        });
       }
-
     }
-  });
+});
   
